ffdddabc87b1a5d0145168108075025f98a9dd92
Ivan Heibi <ivanhb.ita@gmail.com>
2018-03-19 17:30:55 +0100

    new coci rdf gen script

---
+++ script/coci_rdfgen.py
@@ -0,0 +1,48 @@
+
+import os
+from argparse import ArgumentParser
+import citation
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser("coci_rdfgen.py", description="Create the RDF dataset for COCI")
+    arg_parser.add_argument("-in", "--input_dir", dest="input_dir", required=True, help="The root directory of the COCI processed data")
+
+    INPUT_ROOT_DIR = "."
+
+    if args.input_dir:
+        INPUT_ROOT_DIR = args.input_dir
+
+    all_dirs = [ os.path.join(INPUT_ROOT_DIR, name) for name in os.listdir(INPUT_ROOT_DIR) if os.path.isdir(os.path.join(INPUT_ROOT_DIR, name)) ]
+
+
+    print("The Root directory is %s"%(INPUT_ROOT_DIR))
+    for d in all_dirs:
+        print("Processin %s"%(d))
+        data_dir = '%s/data'%(d)
+        prov_dir = '%s/prov'%(d)
+
+        #populate prov dictionary
+        for files in os.walk(prov_dir):
+            for file in files:
+                if file.lower().endswith('.json'):
+                    p_file_path = os.path.join(prov_dir, file)
+                    #open csv and update prov dictionary
+                    with open(p_file_path,'r') as csvfile:
+                        csv_reader = csv.DictReader(csvfile)
+                        for row in csv_reader:
+                            date_dic[row['oci']] = {'agent': row['agent'],'source': row['source'],'datetime': row['datetime']}
+
+        #iterate all data entries
+        for files in os.walk(data_dir):
+            for file in files:
+                if file.lower().endswith('.json'):
+                    d_file_path = os.path.join(data_dir, file)
+                    #open the csv file and process each data entry
+                    with open(d_file_path,'r') as csvfile:
+                        csv_reader = csv.DictReader(csvfile)
+                        for row in csv_reader:
+                            # oci , citing , cited , creation , timespan
+                            # get the prov data from the prov dictionary oci key
+                            # create the rdf and generate the files
+
+    print("Done")
