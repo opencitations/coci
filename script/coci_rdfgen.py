fb8d43feccc23e487f08893c7dd63f13f1c1d1f3
Ivan Heibi <ivanhb.ita@gmail.com>
2018-03-19 17:46:40 +0100

    citation init for coci

--- script/coci_rdfgen.py
+++ script/coci_rdfgen.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
         prov_dir = '%s/prov'%(d)
 
         #populate prov dictionary
+        prov_dic = {}
         for files in os.walk(prov_dir):
             for file in files:
                 if file.lower().endswith('.json'):
@@ -30,7 +31,7 @@ if __name__ == "__main__":
                     with open(p_file_path,'r') as csvfile:
                         csv_reader = csv.DictReader(csvfile)
                         for row in csv_reader:
-                            date_dic[row['oci']] = {'agent': row['agent'],'source': row['source'],'datetime': row['datetime']}
+                            prov_dic[row['oci']] = {'agent': row['agent'],'source': row['source'],'datetime': row['datetime']}
 
         #iterate all data entries
         for files in os.walk(data_dir):
@@ -44,5 +45,13 @@ if __name__ == "__main__":
                             # oci , citing , cited , creation , timespan
                             # get the prov data from the prov dictionary oci key
                             # create the rdf and generate the files
+                            oci_prov = {'agent': None,'source': None,'datetime': None}
+                            if row['oci'] in prov_dic:
+                                oci_prov = prov_dic[row['oci']]
+
+                            citation = Citation( None, 'citing_url', None,
+                                                None, 'cited_url', None,
+                                                oci_prov['agent'], oci_prov['source'],oci_prov['daterime'],
+                                                creation_date= row['creation'], duration= row['timespan'], oci= row['oci'])
 
     print("Done")
