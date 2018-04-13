0d5b641b3618ef1e7ef7b0a0052e8f708ee51050
Ivan Heibi <ivanhb.ita@gmail.com>
2018-04-13 12:38:00 +0200

    coci_blobdate_gen fixed and ready

--- script/coci_globdate_gen.py
+++ script/coci_globdate_gen.py
@@ -4,18 +4,51 @@ from argparse import ArgumentParser
 from citation import Citation
 from rdflib.namespace import RDF, RDFS, SKOS
 import csv
+from datetime import datetime
+
+
+def update_dates(date_val, all_dates):
+    #check length string
+    len_date_val = len(date_val)
+    for date_key in all_dates:
+        len_alldates = len(date_key)
+        break
+    # I have a better date already
+    if len_date_val < len_alldates:
+        return all_dates
+    # I have a new better date
+    elif len_date_val > len_alldates:
+        new_alldates = {}
+        new_alldates[date_val] = 1
+        return new_alldates
+    # New date has equl importance to others
+    elif len_date_val == len_alldates:
+        all_dates[date_val] = 1
+        return all_dates
+
+
+def getdateformat(date_str):
+    str_format = ""
+    if Citation.contains_years(date_str):
+        str_format = str_format + "%Y"
+    if Citation.contains_months(date_str):
+        str_format = str_format + "-%m"
+    if Citation.contains_days(date_str):
+        str_format = str_format + "-%d"
+    return str_format
 
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser("coci_globdate_gen.py", description="Create a global dates index")
     arg_parser.add_argument("-in", "--input_dir", dest="input_dir", required=True, help="The root directory of the COCI processed data")
+    arg_parser.add_argument("-out", "--output", dest="output_file", required=False, help="Path/dir of the output file")
     arg_parser.add_argument("-g", "--input_glob_file", dest="input_glob_file", required=False, help="Path+Name of the global dates index file. In case not specified create the file is empty and is created from zero")
     arg_parser.add_argument("-pindex", dest="pindex", required=False, help="Path+Name of the dirs processed index file")
     args = arg_parser.parse_args()
 
     INPUT_ROOT_DIR = "."
-    OUTPUT_FILE = "globdateindex/globdates.nt"
     PROCESSED_INDEX = "globdateindex/processed.csv"
+    OUTPUT_FILE = "globdateindex/globdates.csv"
 
     if args.input_dir:
         INPUT_ROOT_DIR = args.input_dir
@@ -23,10 +56,27 @@ if __name__ == "__main__":
     if args.pindex:
         PROCESSED_INDEX = args.pindex
 
-    processed_dic = {}
-    if not os.path.exists(os.path.dirname(PROCESSED_INDEX)):
+    if args.output_file:
+        PROCESSED_INDEX = args.output_file+"/processed.csv"
+        OUTPUT_FILE = args.output_file+"/globdates.csv"
+
+    #create output file
+    if not os.path.exists(os.path.dirname(OUTPUT_FILE)):
         try:
-            os.makedirs(os.path.dirname(PROCESSED_INDEX))
+            os.makedirs(os.path.dirname(OUTPUT_FILE))
+            pass
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+    with open(OUTPUT_FILE, 'w') as f:
+        f.write("id,value")
+
+    #create processed file
+    processed_dic = {}
+    if not os.path.exists(PROCESSED_INDEX):
+        try:
+            if not os.path.exists(os.path.dirname(PROCESSED_INDEX)):
+                os.makedirs(os.path.dirname(PROCESSED_INDEX))
             with open(PROCESSED_INDEX, 'w') as f:
                 pass
         except OSError as exc:
@@ -38,24 +88,17 @@ if __name__ == "__main__":
             for row in csv_reader:
                 processed_dic[row['id']] = 1
 
-    print(processed_dic)
 
     #in case not specified create the global dates file is empty and gets created from zero
     glob_date_dic = {}
     if args.input_glob_file:
-        OUTPUT_FILE = args.input_glob_file
-        if not os.path.exists(os.path.dirname(OUTPUT_FILE)):
-            try:
-                os.makedirs(os.path.dirname(OUTPUT_FILE))
-                with open(OUTPUT_FILE, 'w') as f:
-                    pass
-            except OSError as exc:
-                if exc.errno != errno.EEXIST:
-                    raise
+        GLOB_DATES = args.input_glob_file
+        if not os.path.exists(os.path.dirname(GLOB_DATES)):
+            print("Path to global dates not correct!")
+            raise
         else:
-            globdate_file_path = OUTPUT_FILE
             #init the global dictionary
-            with open(globdate_file_path,'r') as csvfile:
+            with open(GLOB_DATES,'r') as csvfile:
                 csv_reader = csv.DictReader(csvfile)
                 for row in csv_reader:
                     doi_key = row['id']
@@ -81,8 +124,9 @@ if __name__ == "__main__":
                         if date_val in glob_date_dic[doi_key]:
                             glob_date_dic[doi_key][date_val] = glob_date_dic[doi_key][date_val] + 1
                         else:
+                            print(glob_date_dic[doi_key])
+                            print(date_val)
                             glob_date_dic[doi_key] = update_dates(date_val, glob_date_dic[doi_key])
-                            #check my date compared with other dates
                     else:
                         glob_date_dic[doi_key] = {}
                         glob_date_dic[doi_key][date_val] = 1
@@ -91,75 +135,31 @@ if __name__ == "__main__":
         with open(PROCESSED_INDEX, 'a', newline='') as f:
             f.write(d)
     print("Done")
-    print(glob_date_dic)
-
-def update_dates(date_val, all_dates):
-
-    if date_val in all_dates:
-        all_dates[date_val] = all_dates[date_val] + 1
-        return all_dates
-
-    all_dates_years = []
-    all_dates_months = []
-    all_dates_days = []
-    for date_key in all_dates:
-        i_date = get_date_parts(date_key)
-        all_dates_years.append(i_date.year)
-        all_dates_months.append(i_date.month)
-        all_dates_days.append(i_date.day)
-
-    my_date = get_date_parts(date_val)
-
-    yearindex = all_dates_years.index(my_date.year)
-    if yearindex != -1:
-        com_res = best_date(my_date.month, all_dates_months[yearindex])
-        if com_res == 1:
-            all_dates.pop(rebuild_date_parts(all_dates_years[yearindex], all_dates_months[yearindex], all_dates_days[yearindex]), None)
-            all_dates[date_val] = 1
-        elif com_res == 2:
-            pass
-        elif com_res == 0:
-            com_res = best_date(my_date.day, all_dates_days[yearindex])
-            if com_res == 1:
-                all_dates.pop(rebuild_date_parts(all_dates_years[yearindex], all_dates_months[yearindex], all_dates_days[yearindex]), None)
-                all_dates[date_val] = 1
-            elif com_res == 2:
-                pass
-            elif com_res == 0:
-                all_dates[date_val] = all_dates[date_val] + 1
-
-    return all_dates
-
-    def best_date(date_a, date_b):
-        if date_a == date_b:
-            return 0
-        elif date_a == None and date_b != None :
-            return 2
-        else:
-            return 1
-
-def rebuild_date_parts(year,month,day):
-    str_date = ""
-    if year != None:
-        str_date = year
-        if month != None:
-            str_date = str_date + "-" + month
-            if day != None:
-                str_date = str_date + "-" + day
-    return str_date
+    #print(glob_date_dic)
 
 
-def get_date_parts(date_val):
-    year = None
-    if Citation.contains_years(date_val):
-        year = date_val[0:4]
+    #get best date from the dates list of each doi
+    block_txt = ""
+    for doikey in glob_date_dic:
+        bestdate = []
+        maxscore = 0
+        for datekey in glob_date_dic[doikey]:
+            if glob_date_dic[doikey][datekey] > maxscore:
+                maxscore = glob_date_dic[doikey][datekey]
+                bestdate = [datekey]
+            elif glob_date_dic[doikey][datekey] == maxscore:
+                bestdate.append(datekey)
+        # choose only 1 date from the list
+        # the most recent date
+        choosen_date = "1000-01-01"
+        for d in bestdate:
+            d_datetime = datetime.strptime(d, getdateformat(d))
+            choosen_date_datetime = datetime.strptime(choosen_date, getdateformat(choosen_date))
+            if d_datetime > choosen_date_datetime:
+                choosen_date = d
+        #update dictionary with the choosen date
+        #glob_date_dic[doikey] = choosen_date
+        block_txt = block_txt + '\n"%s",%s'%(doikey.replace('"', '""'),choosen_date)
 
-    month = None
-    if Citation.contains_months(date_val):
-        month = date_val[6:7]
-
-    day = None
-    if Citation.contains_days(date_val):
-        day = date_val[9:10]
-
-    return {"year": year, "month": month, "day":day}
+    with open(OUTPUT_FILE, 'a', newline='') as f:
+        f.write(block_txt)
