5ba83f096a68625346416ae5b1b2b2218df475dc
ivan.heibi <ivan.heibi@MacBook-Pro-di-apple.local>
2018-07-04 11:34:32 +0200

    new rdfgen

--- script/coci_rdfgen.py
+++ script/coci_rdfgen.py
@@ -57,55 +57,55 @@ if __name__ == "__main__":
 
 
     print("The Root directory is %s"%(INPUT_ROOT_DIR))
-    for d in all_dirs:
-        print("Processin %s"%(d))
-        data_dir = '%s/data/'%(d)
-        prov_dir = '%s/prov/'%(d)
 
-        #populate prov dictionary
-        prov_dic = {}
-        for dirpath, dnames, fnames in os.walk(prov_dir):
-            for f in fnames:
-                if f.lower().endswith('.csv'):
-                    p_file_path = os.path.join(prov_dir, f)
-                    #open csv and update prov dictionary
-                    with open(p_file_path,'r') as csvfile:
-                        csv_reader = csv.DictReader(csvfile)
-                        for row in csv_reader:
-                            prov_dic[row['oci']] = {'agent': row['agent'],'source': row['source'],'datetime': row['datetime']}
+    #print("Processin %s"%(d))
+    data_dir = '%s/data/'%(INPUT_ROOT_DIR)
+    prov_dir = '%s/prov/'%(INPUT_ROOT_DIR)
 
-        #iterate all data entries
-        for dirpath, dnames, fnames in os.walk(data_dir):
-            for f in fnames:
-                if f.lower().endswith('.csv'):
-                    d_file_path = os.path.join(data_dir, f)
+    #populate prov dictionary
+    prov_dic = {}
+    for dirpath, dnames, fnames in os.walk(prov_dir):
+        for f in fnames:
+            if f.lower().endswith('.csv'):
+                p_file_path = os.path.join(prov_dir, f)
+                #open csv and update prov dictionary
+                with open(p_file_path,'r') as csvfile:
+                    csv_reader = csv.DictReader(csvfile)
+                    for row in csv_reader:
+                        prov_dic[row['oci']] = {'agent': row['agent'],'source': row['source'],'datetime': row['datetime']}
 
-                    #open the csv file and process each data entry
-                    block_txt = ""
-                    with open(d_file_path,'r') as csvfile:
-                        csv_reader = csv.DictReader(csvfile)
-                        for row in csv_reader:
-                            # oci , citing , cited , creation , timespan
-                            # get the prov data from the prov dictionary oci key
-                            # create the rdf and generate the files
-                            oci_prov = {'agent': None,'source': None,'datetime': None}
-                            if row['oci'] in prov_dic:
-                                oci_prov = prov_dic[row['oci']]
+    #iterate all data entries
+    for dirpath, dnames, fnames in os.walk(data_dir):
+        for f in fnames:
+            if f.lower().endswith('.csv'):
+                d_file_path = os.path.join(data_dir, f)
 
-                            ## replace sorce call
-                            prov_query = urllib.parse.quote(oci_prov['source'].replace(prov_source, ''))
-                            oci_prov['source'] = prov_source + prov_query
+                #open the csv file and process each data entry
+                block_txt = ""
+                with open(d_file_path,'r') as csvfile:
+                    csv_reader = csv.DictReader(csvfile)
+                    for row in csv_reader:
+                        # oci , citing , cited , creation , timespan
+                        # get the prov data from the prov dictionary oci key
+                        # create the rdf and generate the files
+                        oci_prov = {'agent': None,'source': None,'datetime': None}
+                        if row['oci'] in prov_dic:
+                            oci_prov = prov_dic[row['oci']]
 
-                            #print(row)
-                            citation = Citation( None, doi_prefix +  urllib.parse.quote(row['citing']), None,
-                                                None, doi_prefix +  urllib.parse.quote(row['cited']), None,
-                                                agent_url, oci_prov['source'], oci_prov['datetime'],
-                                                creation_date= row['creation'], duration= row['timespan'], oci= row['oci'])
+                        ## replace sorce call
+                        prov_query = urllib.parse.quote(oci_prov['source'].replace(prov_source, ''))
+                        oci_prov['source'] = prov_source + prov_query
 
-                            g = citation.get_citation_rdf(BASE_URL, include_oci=False, include_id_link=False, include_rdfs_lbl=False, include_data=INCLUDE_DATA, include_prov=INCLUDE_PROV)
-                            block_txt = block_txt + g.serialize(format='nt').decode("utf-8")
+                        #print(row)
+                        citation = Citation( None, doi_prefix +  urllib.parse.quote(row['citing']), None,
+                                            None, doi_prefix +  urllib.parse.quote(row['cited']), None,
+                                            agent_url, oci_prov['source'], oci_prov['datetime'],
+                                            creation_date= row['creation'], duration= row['timespan'], oci= row['oci'])
 
-                    if block_txt != "":
-                        with open(OUTPUT_FILE, 'a', newline='') as f:
-                            f.write(block_txt)
+                        g = citation.get_citation_rdf(BASE_URL, include_oci=False, include_id_link=False, include_rdfs_lbl=False, include_data=INCLUDE_DATA, include_prov=INCLUDE_PROV)
+                        block_txt = block_txt + g.serialize(format='nt').decode("utf-8")
+
+                if block_txt != "":
+                    with open(OUTPUT_FILE, 'a', newline='') as f:
+                        f.write(block_txt )
     print("Done")
